42e4fc64e66f4d9471568387af611480338455eb
Aldo Febrian <aldo_zou96@yahoo.com>
2017-04-10 19:36:36 +0700

    Connect Server Complete

--- app/src/main/java/id/ac/umn/onc/mydiary/LoginActivity.java
+++ app/src/main/java/id/ac/umn/onc/mydiary/LoginActivity.java
@@ -4,23 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import id.ac.umn.onc.mydiary.clientSide.socket.Message;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginButton;
+    EditText inputEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginButton = (Button) findViewById(R.id.btnLogin);
+        inputEdit = (EditText) findViewById(R.id.txtInput);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SelectHostActivity.client.send("New User");
-                //Toast.makeText(LoginActivity.this, "Login Login", Toast.LENGTH_SHORT).show();
+                if(inputEdit.getText().equals("")){
+                    inputEdit.setError("Jangan Kosong !");
+                }
+                else {
+                    SelectHostActivity.client.send(inputEdit.getText().toString());
+                    inputEdit.setText("");
+                }
+
             }
         });
     }
---
+++ app/src/main/java/id/ac/umn/onc/mydiary/clientSide/socket/ClientSocket.java
@@ -0,0 +1,57 @@
+package id.ac.umn.onc.mydiary.clientSide.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import id.ac.umn.onc.mydiary.SelectHostActivity;
+
+/**
+ * Created by aldo_ on 10/04/2017.
+ */
+
+public class ClientSocket  implements Runnable{
+    public int port;
+    public String serverAddr;
+    public Socket socket;
+    public SelectHostActivity ui;
+    public DataInputStream In;
+    public DataOutputStream Out;
+
+    public ClientSocket(SelectHostActivity ui) throws IOException {
+        this.ui = ui;
+        this.serverAddr = ui.addrEdit.getText().toString();
+        this.port = Integer.parseInt(ui.portEdit.getText().toString());
+        socket = new Socket(InetAddress.getByName(serverAddr),port);
+        Out = new DataOutputStream(socket.getOutputStream());
+        In = new DataInputStream(socket.getInputStream());
+        Out.flush();
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try{
+                String msg = In.readUTF();
+                System.out.println("Incoming : "+msg);
+                Thread.sleep(1000);
+            }
+            catch (Exception e){
+                System.out.println("Something wrong");
+
+            }
+        }
+    }
+
+    public void send(String msg){
+        try {
+            Out.writeUTF(msg);
+            System.out.println("Outgoing : "+msg);
+        }
+        catch (Exception ex) {
+            System.out.println("Message Error");
+        }
+    }
+}
