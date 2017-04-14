6e6a180ac0066bf63382676af141fde11236e5cf
Aldo Febrian <aldo_zou96@yahoo.com>
2017-04-14 11:46:42 +0700

    Fix Manifest

--- app/src/main/java/id/ac/umn/onc/mydiary/activity/HomeActivity.java
+++ app/src/main/java/id/ac/umn/onc/mydiary/activity/HomeActivity.java
@@ -3,10 +3,14 @@ package id.ac.umn.onc.mydiary.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 import id.ac.umn.onc.mydiary.R;
+import id.ac.umn.onc.mydiary.model.Memo;
 
 public class HomeActivity extends AppCompatActivity {
-
+    ArrayList<Memo> memos = new ArrayList<>();
+    ArrayList<String> memosName = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
